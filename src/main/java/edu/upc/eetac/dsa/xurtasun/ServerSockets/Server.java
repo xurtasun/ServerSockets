c79dcc4fd023cdd3074d81fe16167cbb77c9f7c1
xurtasun <xavi.urta@gmail.com>
2014-10-05 17:54:12 +0200

    1

---
+++ src/main/java/edu/upc/eetac/dsa/xurtasun/ServerSockets/Server.java
@@ -0,0 +1,41 @@
+package edu.upc.eetac.dsa.xurtasun.ServerSockets;
+
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        
+
+        int portNumber = 2001;
+
+        try ( 
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+        
+            String inputLine, outputLine;
+            System.out.println("iniciando");
+            // Initiate conversation with client
+            ServerProtocol kkp = new ServerProtocol();
+            outputLine = kkp.processInput(null);
+            out.println(outputLine);
+
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = kkp.processInput(inputLine);
+                out.println(outputLine);
+                if (outputLine.equals("Bye."))
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+}
+   
